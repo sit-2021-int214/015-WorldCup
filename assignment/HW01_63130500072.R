34bd327fb0184d1723315bddd72f75fd12fc294f
Nithi1701 <72918129+Nithi1701@users.noreply.github.com>
2021-08-23 19:06:36 +0700

    Add files via upload

---
+++ assignment/HW01_63130500072.R
@@ -0,0 +1,24 @@
+# Exercise 1
+x <- c(10.4, 5.6, 3.1, 6.4, 21.7)
+mean(x) #9.44
+sum(x) #47.2
+median(x) #6.4
+sd(x) #7.33846
+var(x) #53.853
+# Exercise 2
+
+names <- c("Iron Man","The Incredible Hulk","Iron Man 2","Thor","Captain America: The First Avenger",
+           "The Avengers","Iron Man 3","Thor: The Dark World","Captain America: The Winter Soldier",
+           "Guardians of the Galaxy","Avengers: Age of Ultron","Ant-Man","Captain America: Civil War",
+           "Doctor Strange","Guardians of the Galaxy 2","Spider-Man: Homecoming","Thor: Ragnarok","Black Panther",
+           "Avengers: Infinity War","Ant-Man and the Wasp","Captain Marvel","Avengers: Endgame",
+           "Spider-Man: Far From Home","WandaVision","Falcon and the Winter Soldier","Loki","Black Widow")
+years <- c(2008,2008,2010,2011,2011,2012,2013,2013,2014,2014,2015,2015,2016,2016,
+           2017,2017,2017,2017,2018,2018,2019,2019,2019,2021,2021,2021,2021)
+years <- c(2008,2008,2010,2011,2011,2012,rep(2013:2016,each=2),
+           rep(2017,4),rep(2018,2),rep(2019,3),rep(2021,4))
+length(names) #27
+names[19] #"Avengers: Infinity War"
+count <- table(years)
+count[which(count == max(count))] #2017 ,2021
+
